dd45a7f8e4c869ce352d90d774f9c3e12d10f748
Yoann Kergall <yoann.kergall@gmail.com>
2020-02-18 21:54:41 +0100

    creation HelloController

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld/ 
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // GET: HelloWorld/Welcome/Scott/1
+        public string Welcome(int id = 1, string name = "toto")
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + id);
+        }
+    }
+}
